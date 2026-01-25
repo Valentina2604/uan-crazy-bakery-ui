8a560e42c4a51ddd8eb5e03728e0ee4d8a4cf6b8
johnnyfabianmallama <johnnyfabianmallama@gmail.com>
2026-01-25 05:53:29 +0000

    Ajustes

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'oaidalleapiprodscus.blob.core.windows.net',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
